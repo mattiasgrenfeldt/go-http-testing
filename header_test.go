e185628c0236c3e3bca14e545f8715831ef04afe
Mattias Grenfeldt <grenfeldt@google.com>
2020-06-30 09:38:36 +0200

    Header testing setup

---
+++ header_test.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"context"
+	"io"
+	"net/http"
+	"testing"
+)
+
+type ToyHandler struct {
+	Req  *http.Request
+	Done chan bool
+}
+
+func (h *ToyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	h.Req = r
+	io.WriteString(w, "Cool")
+	h.Done <- true
+}
+
+func performRequest(request string) (*http.Request, error) {
+	done := make(chan bool, 1)
+	handler := &ToyHandler{
+		Req:  nil,
+		Done: done}
+
+	srv := http.Server{Handler: handler}
+	listener := NewToyListener(request)
+
+	var err error
+	go func() {
+		err = srv.Serve(&listener)
+		if err != http.ErrServerClosed {
+			done <- true
+		}
+	}()
+	<-done
+	srv.Shutdown(context.Background())
+
+	if err != http.ErrServerClosed {
+		return handler.Req, err
+	}
+
+	return handler.Req, nil
+}
+
+func TestBasic(t *testing.T) {
+	request := "GET / HTTP/1.1\r\n" +
+		"Host: localhost:8080\r\n" +
+		"A: B\r\n" +
+		"\r\n"
+
+	req, err := performRequest(request)
+	if err != nil {
+		t.Errorf("ERROR: %v", err)
+	}
+
+	headers := req.Header
+	if len(headers["A"]) != 1 || headers["A"][0] != "B" {
+		t.Errorf("got:, %v", headers)
+	}
+}
+
+func TestOrdering(t *testing.T) {
+	request := "GET / HTTP/1.1\r\n" +
+		"Host: localhost:8080\r\n" +
+		"A: X\r\n" +
+		"A: Y\r\n" +
+		"\r\n"
+
+	req, err := performRequest(request)
+	if err != nil {
+		t.Errorf("ERROR: %v", err)
+	}
+
+	headers := req.Header
+	if len(headers["A"]) != 2 || headers["A"][0] != "X" || headers["A"][1] != "Y" {
+		t.Errorf("got:, %v", headers)
+	}
+}
---
+++ toylistener.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"errors"
+	"net"
+	"sync"
+)
+
+// ToyListener implements net.Listener
+type ToyListener struct {
+	s2c          net.Conn
+	c2s          net.Conn
+	wg           *sync.WaitGroup
+	acceptedOnce bool
+	request      string
+}
+
+// NewToyListener creates a new ToyListener that is only in memory.
+func NewToyListener(request string) ToyListener {
+	s2c, c2s := net.Pipe()
+
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+	return ToyListener{
+		s2c:          s2c,
+		c2s:          c2s,
+		wg:           wg,
+		acceptedOnce: false,
+		request:      request}
+}
+
+// Accept waits for and returns the next connection to the listener.
+func (l *ToyListener) Accept() (net.Conn, error) {
+	if l.acceptedOnce {
+		l.wg.Wait()
+		return l.s2c, errors.New("listener is closed")
+	}
+
+	go func() {
+		l.c2s.Write([]byte(l.request))
+	}()
+	l.acceptedOnce = true
+	return l.s2c, nil
+}
+
+// Close closes the listener.
+// Any blocked Accept operations will be unblocked and return errors.
+func (l *ToyListener) Close() error {
+	l.s2c.Close()
+	l.c2s.Close()
+	l.wg.Done()
+	return nil
+}
+
+// Addr returns the listener's network address.
+func (l *ToyListener) Addr() net.Addr {
+	return l.c2s.LocalAddr()
+}
